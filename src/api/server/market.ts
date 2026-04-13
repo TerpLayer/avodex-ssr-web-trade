e55a0773647f8661dc6ee6f53194a3855b132b4c
james <stavrogin@163.com>
2026-04-13 22:25:09 +0800

    release 20260413_01

--- src/api/server/market.ts
+++ src/api/server/market.ts
@@ -11,3 +11,9 @@ const URL = getServerSideDomain({
 export function get_symbol(config?) {
   return AzAxios4.get(URL + `symbol`, config);
 }
+
+//获取交易对
+export function get_builder_symbol(config?) {
+  const domain = process.env.NEXT_PUBLIC_ENV ? "https://app.azverse.xyz" : "https://app.az-qa.xyz";
+  return AzAxios4.get(`${domain}/sapi/v4/market/public/symbol`, config);
+}
